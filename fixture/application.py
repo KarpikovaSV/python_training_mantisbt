b473855fa86a49b0a1ba0651198c897ce18c7ffd
Svetlana K <>
2022-02-01 18:04:33 +0300

    Вход в систему багтрек

---
+++ fixture/application.py
@@ -0,0 +1,59 @@
+from selenium import webdriver
+# from selenium.common.exceptions import NoSuchElementException
+# from selenium.common.exceptions import NoAlertPresentException
+from fixture.session import SessionHelper
+
+
+
+class Application:
+
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+        #self.wd = webdriver.Firefox()
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "safari":
+            self.wd = webdriver.Safari()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+        # Без задержки валятся тесты
+        self.wd.implicitly_wait(3)
+        self.session = SessionHelper(self)
+        self.base_url = base_url
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
+    def open_home_page(self):
+        wd = self.wd
+        if not wd.current_url.endswith("/index.php"):
+            wd.get(self.base_url)
+
+    def open_page(self):
+        wd = self.wd
+        if not (wd.current_url.endswith("/edit.php") and len(wd.find_elements_by_name("photo"))) > 0:
+            #wd.get("http://localhost/addressbook/edit.php")
+            bu = self.base_url
+            wd.get(bu + "/edit.php")
+
+    def is_element_present(self, how, what):
+        try:
+            self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+    def is_alert_present(self):
+        try:
+            self.wd.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
+
+    def destroy(self):
+        self.wd.quit()
---
+++ fixture/session.py
@@ -0,0 +1,54 @@
+
+
+class SessionHelper:
+    def __init__(self, app):
+        self.app = app
+
+    def login(self, username, password):
+        # login
+        wd = self.app.wd
+        self.app.open_home_page()
+        wd.find_element_by_name("username").clear()
+        wd.find_element_by_name("username").send_keys(username)
+        wd.find_element_by_name("username").click()
+        wd.find_element_by_xpath(u"//input[@value='Вход']").click()
+        wd.find_element_by_name("password").clear()
+        wd.find_element_by_name("password").send_keys(password)
+        wd.find_element_by_xpath(u"//input[@value='Вход']").click()
+        #wd.find_element_by_id("LoginForm").click()
+        #wd.find_element_by_css_selector('input[type="submit"]')
+
+    def ensure_login(self, username, password):
+        wd = self.app.wd
+        if self.is_logget_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, password)
+
+    def is_logged_in_as(self, username):
+        wd = self.app.wd
+        return self.get_logget_user() == username
+
+    def get_logget_user(self):
+        wd = self.app.wd
+        return wd.find_element_by_css_selector("span.user-info").text
+
+    def logout(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("Logout").click()
+        wd.find_element_by_name("user")
+
+    def ensure_logout(self):
+        wd = self.app.wd
+        if self.is_logget_in():
+            self.logout()
+
+    def is_logget_in(self):
+        wd = self.app.wd
+        return len(wd.find_elements_by_link_text("Logout")) > 0
+
+
+
+
