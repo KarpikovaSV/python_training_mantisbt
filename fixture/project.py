9342d07792592b5bc2b82626a4582f2aab44b395
Svetlana K <>
2022-02-09 14:36:36 +0300

    Добавлен тест удаления нового проекта

--- fixture/project.py
+++ fixture/project.py
@@ -1,6 +1,5 @@
 from selenium.webdriver.support.ui import Select
 from model.project import Project
-import re
 import random
 import string
 import time
@@ -24,76 +23,30 @@ class ProjectHelper:
         symbols = string.ascii_letters + string.digits
         return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-    def delete_first_contact(self):
+    def select_project_by_name(self, name):
         wd = self.app.wd
-        self.delete_contact_by_index(0)
+        table = wd.find_element_by_css_selector("table.table-striped.table-bordered.table-condensed.table-hover")
+        tbody = table.find_element(By.TAG_NAME, "tbody")
+        for element in tbody.find_elements(By.TAG_NAME, "a"):
+            if name == element.text:
+                element.click()
+                return
 
-    def delete_contact_by_index(self, index):
+    def delete_project_by_name(self, name):
         wd = self.app.wd
         self.app.open_home_page()
-        self.select_contact_by_index(index)
-        wd.find_element_by_xpath("//input[@value='Delete']").click()
-        #wd.switch_to_alert().accept()
+        self.select_project_by_name(name)
+        wd.find_element_by_xpath(u"//input[@value='Удалить проект']").click()
+        time.sleep(3)
+        wd.find_element_by_xpath(u"//input[@value='Удалить проект']").click()
         self.contact_cache = None
-
-    def select_contact_by_index(self, index):
-        wd = self.app.wd
-        #wd.find_elements_by_name("selected[]")[index].click()
-        wd.find_elements_by_xpath("//img[@alt='Edit']")[index].click()
-
-    def select_contact_by_select_id(self, id):
-        wd = self.app.wd
-        wd.find_element_by_xpath("//input[@value='%s']" % id).click()
-
-    def select_contact_by_id(self, id):
-        wd = self.app.wd
-        #wd.find_elements_by_name("selected[]")[index].click()
-        #s_id = "input[value='%s']" % id
-        wd.find_element_by_xpath("//a[@href='edit.php?id=%s']" % id).click()
-
-    def delete_contact_by_id(self, id):
-        wd = self.app.wd
-        self.app.open_home_page()
-        self.select_contact_by_id(id)
-        wd.find_element_by_xpath("//input[@value='Delete']").click()
-        self.contact_cache = None
-
-    def select_first_contacts(self):
-        wd = self.app.wd
-        wd.find_elements_by_name("selected[]").click()
 
     def count_project(self):
         wd = self.app.wd
         self.app.open_home_page()
-        return len(wd.find_elements_by_name("selected[]"))
-
-    def modify_first_contact(self):
-        wd = self.app.wd
-        self.modify_contact_by_index(0)
-
-    def modify_contact_by_index(self, index, new_contact_data):
-        wd = self.app.wd
-        self.app.open_home_page()
-        self.select_contact_by_index(index)
-        # fill form
-        self.fill_contact(new_contact_data)
-        # submit modification
-        wd.find_element_by_name("update").click()
-        self.contact_cache = None
-
-    def modify_contact_by_id(self, id, new_contact_data):
-        wd = self.app.wd
-        self.app.open_home_page()
-        self.select_contact_by_id(id)
-        # fill form
-        self.fill_contact(new_contact_data)
-        # submit modification
-        wd.find_element_by_name("update").click()
-        self.contact_cache = None
-
-    def select_first_contact(self):
-        wd = self.app.wd
-        wd.find_element_by_xpath("//img[@alt='Edit']").click()
+        table = wd.find_element_by_css_selector("table.table-striped.table-bordered.table-condensed.table-hover")
+        tbody = table.find_element(By.TAG_NAME, "tbody")
+        return len(tbody.find_elements(By.TAG_NAME, "a"))
 
     def fill_name_project(self, project):
         wd = self.app.wd
@@ -105,13 +58,6 @@ class ProjectHelper:
             wd.find_element_by_id(field_name).click()
             wd.find_element_by_id(field_name).clear()
             wd.find_element_by_id(field_name).send_keys(text)
-
-    def count(self):
-        wd = self.app.wd
-        self.app.open_home_page()
-        return len(wd.find_elements_by_name("selected[]"))
-
-    contact_cache = None
 
     def get_project_list(self):
         if self.contact_cache is None:
@@ -130,61 +76,6 @@ class ProjectHelper:
         if text is not None:
             wd.find_element_by_name(field_name).click()
             Select(wd.find_element_by_name(field_name)).select_by_visible_text(text)
-
-    def open_contact_to_edit_by_index(self, index):
-        wd = self.app.wd
-        self.app.open_home_page()
-        self.select_contact_by_index(index)
-
-    def open_contact_view_by_index(self, index):
-        wd = self.app.wd
-        self.app.open_home_page()
-        wd.find_elements_by_xpath("//img[@alt='Details']")[index].click()
-
-    def get_contact_info_from_edit_page(self, index):
-        wd = self.app.wd
-        self.open_contact_to_edit_by_index(index)
-        firstname = wd.find_element_by_name("firstname").get_attribute("value")
-        lastname = wd.find_element_by_name("lastname").get_attribute("value")
-        id = wd.find_element_by_name("id").get_attribute("value")
-        address = wd.find_element_by_name("address").get_attribute("value")
-        email = wd.find_element_by_name("email").get_attribute("value")
-        email2 = wd.find_element_by_name("email2").get_attribute("value")
-        email3 = wd.find_element_by_name("email3").get_attribute("value")
-        homephone = wd.find_element_by_name("home").get_attribute("value")
-        workphone = wd.find_element_by_name("work").get_attribute("value")
-        mobilephone = wd.find_element_by_name("mobile").get_attribute("value")
-        secondaryphone = wd.find_element_by_name("phone2").get_attribute("value")
-        return Contact(firstname=firstname, lastname=lastname, id = id, address=address, email=email, email2=email2, email3=email3,
-                       homephone=homephone, workphone=workphone, mobilephone=mobilephone, secondaryphone=secondaryphone)
-
-    def get_contact_from_view_page(self, index):
-        wd = self.app.wd
-        self.open_contact_view_by_index(index)
-        text = wd.find_element_by_id("content").text
-        homephone = re.search("H: (.*)", text).group(1)
-        workphone = re.search("W: (.*)", text).group(1)
-        mobilephone = re.search("M: (.*)", text).group(1)
-        secondaryphone = re.search("P: (.*)", text).group(1)
-        return Contact(homephone=homephone, workphone=workphone, mobilephone=mobilephone, secondaryphone=secondaryphone)
-
-    def del_contact_by_id_in_group(self, id, id_group):
-        wd = self.app.wd
-        self.app.open_home_page()
-        # тут посмотреть название крутежка
-        wd.find_element_by_name("group").click()
-        # тут выбор группы по id
-        Select(wd.find_element_by_name("group")).select_by_value(id_group)
-        # выбор контакта
-        self.select_contact_by_select_id(id)
-        wd.find_element_by_name("remove").click()
-        self.contact_cache = None
-
-    def select_group_by_select_id(self, id):
-        wd = self.app.wd
-        wd.find_element_by_xpath("//input[@value='%s']" % id).click()
-
-
 
 
 
---
+++ test/test_del_project.py
@@ -0,0 +1,17 @@
+from model.project import Project
+import random
+
+
+def test_del_project(app):
+    if app.project.count_project() == 0:
+        name = app.project.random("name", 10)
+        project = Project(name=name)
+        app.project.create_project(project)
+    old_projects = app.project.get_project_list()
+    project = random.choice(old_projects)
+    app.project.delete_project_by_name(project.name)
+    new_projects = app.project.get_project_list()
+    assert len(old_projects) - 1 == app.project.count_project()
+    old_projects.remove(project)
+    assert old_projects == new_projects
+    assert sorted(old_projects, key=Project.name_or_empty) == sorted(new_projects, key=Project.name_or_empty)
